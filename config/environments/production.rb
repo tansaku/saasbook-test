61a00051ded8ab06570e27a0d27dea146ab28df7
Sam Joseph <tansaku@gmail.com>
2014-08-08 00:22:34 -0300

    seeing if this fixes apple-touch-icon isn't precompiled issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ SaasbookTest::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  config.assets.precompile += %w( public.js public.css  )
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
